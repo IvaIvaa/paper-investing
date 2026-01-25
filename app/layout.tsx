70147e49aedee81408f9c67666ac8a0fc3aa2b1a
IvaIvaa <chikhladzeiva@gmail.com>
2026-01-25 03:33:32 +0100

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -16,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-sans bg-gray-50 text-gray-900`}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
